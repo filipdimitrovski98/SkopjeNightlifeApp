76b841377d89a6eabeebbfaaeac95347d0f488e9
FilipDimitrovski <filip_d_sk@hotmail.com>
2021-01-29 07:27:31 -0800

    Initial Commit

---
+++ SkopjeNightlife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkopjeNightlife
+//
+//  Created by Filip Dimitrovski on 1/29/21.
+//  Copyright © 2021 Filip Dimitrovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
